124241fbb453e877b11c503246a28aa1f902531e
rossella <rsblendido@suse.com>
2015-07-06 01:02:12 +0200

    Don't sync all devices when some fail
    With the new RPC calls get_devices_details_and_failed_devices
    and update_device_list the agent gets a list of devices
    for which some operation failed. The agent can now make use
    of this information and instead of syncing all the devices
    can sync only those which failed.
    With the current change if a device keeps failing, the agent
    will try to sync it forever. In a following patch I will limit
    the number of retrials.
    
    Partially-Implements: blueprint restructure-l2-agent
    Change-Id: I295dc79031a0547f8687c5835c7ba7bbc43df36d
    (cherry picked from commit 584faf47e28568bc521d40988eb42458e1580820)
    
    Conflicts:
    	neutron/tests/functional/agent/l2/base.py
    	neutron/tests/unit/plugins/ml2/drivers/openvswitch/agent/test_ovs_tunnel.py

--- neutron/tests/unit/plugins/ml2/drivers/openvswitch/agent/test_ovs_tunnel.py
+++ neutron/tests/unit/plugins/ml2/drivers/openvswitch/agent/test_ovs_tunnel.py
@@ -530,11 +530,15 @@ class TunnelTest(object):
         self._verify_mock_calls()
 
     def test_daemon_loop(self):
-        reply_ge_1 = {'added': set(['tap0']),
-                      'removed': set([])}
+        reply_ge_1 = {'added': [{'name': 'tap0', 'ofport': 3,
+                                 'external_ids': {
+                                     'attached-mac': 'test_mac'}}],
+                      'removed': []}
 
-        reply_ge_2 = {'added': set([]),
-                  'removed': set(['tap0'])}
+        reply_ge_2 = {'added': [],
+                      'removed': [{'name': 'tap0', 'ofport': 3,
+                                   'external_ids': {
+                                       'attached-mac': 'test_mac'}}]}
 
         reply_pe_1 = {'current': set(['tap0']),
                       'added': set(['tap0']),
@@ -569,13 +573,7 @@ class TunnelTest(object):
                                   'process_ports_events') as process_p_events,\
                 mock.patch.object(
                     self.mod_agent.OVSNeutronAgent,
-                    'scan_ancillary_ports') as scan_ancillary_ports,\
-                mock.patch.object(
-                    self.mod_agent.OVSNeutronAgent,
                     'process_network_ports') as process_network_ports,\
-                mock.patch.object(
-                    self.mod_agent.OVSNeutronAgent,
-                    'process_ancillary_network_ports') as process_anc_ports,\
                 mock.patch.object(self.mod_agent.OVSNeutronAgent,
                                   'tunnel_sync'),\
                 mock.patch.object(time, 'sleep'),\
@@ -584,9 +582,6 @@ class TunnelTest(object):
                     'update_stale_ofport_rules') as update_stale:
             log_exception.side_effect = Exception(
                 'Fake exception to get out of the loop')
-            scan_ancillary_ports.return_value = {
-                'current': set([]), 'added': set([]), 'removed': set([]),
-            }
             update_stale.return_value = []
             devices_not_ready = set()
             process_p_events.side_effect = [
@@ -594,9 +589,11 @@ class TunnelTest(object):
                 (reply_pe_2, reply_ancillary, devices_not_ready)]
             interface_polling = mock.Mock()
             interface_polling.get_events.side_effect = [reply_ge_1, reply_ge_2]
+            failed_devices = {'removed': set([]), 'added': set([])}
+            failed_ancillary_devices = {'removed': set([]), 'added': set([])}
             process_network_ports.side_effect = [
-                False, Exception('Fake exception to get out of the loop')]
-            process_anc_ports.return_value = False
+                failed_devices,
+                Exception('Fake exception to get out of the loop')]
 
             n_agent = self._build_agent()
 
@@ -613,8 +610,10 @@ class TunnelTest(object):
             log_exception.assert_called_once_with(
                 "Error while processing VIF ports")
             process_p_events.assert_has_calls([
-                mock.call(reply_ge_1, set(), set(), devices_not_ready, set()),
+                mock.call(reply_ge_1, set(), set(), devices_not_ready,
+                          failed_devices, failed_ancillary_devices, set()),
                 mock.call(reply_ge_2, set(['tap0']), set(), devices_not_ready,
+                          failed_devices, failed_ancillary_devices,
                           set())
             ])
             process_network_ports.assert_has_calls([
