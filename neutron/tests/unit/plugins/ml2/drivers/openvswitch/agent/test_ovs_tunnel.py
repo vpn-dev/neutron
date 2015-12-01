e14ebf5ecbe46a380734c1f4f0b7450936c7af01
rossella <rsblendido@suse.com>
2015-12-01 16:58:44 +0000

    Revert "Revert "OVS agent reacts to events instead of polling""
    The original change had to be reverted because it caused
    tests failing in the gate. The failures were due to the
    fact that when a port was not ready, an exception was
    thrown to trigger a full resync of the agent. This
    behavior was meant to be temporary and was fixed in a
    dependent patch that was not merged though. This revert
    moves the handling of not ready ports in this patch.
    It also refactors the unit tests a bit.
    
    This reverts commit e7270d9505fe09ec6d687e7796a814bf883b5416.
    
    Change-Id: I6574cef3c95525ace6a98cf968ee159190681394
    (cherry picked from commit ccdf211b4cf224d415520c7d70b7f53952674414)
    
    Conflicts:
    	neutron/tests/functional/agent/l2/base.py
    	neutron/tests/unit/plugins/ml2/drivers/openvswitch/agent/test_ovs_neutron_agent.py
    	neutron/tests/unit/plugins/ml2/drivers/openvswitch/agent/test_ovs_tunnel.py

--- neutron/tests/unit/plugins/ml2/drivers/openvswitch/agent/test_ovs_tunnel.py
+++ neutron/tests/unit/plugins/ml2/drivers/openvswitch/agent/test_ovs_tunnel.py
@@ -530,13 +530,23 @@ class TunnelTest(object):
         self._verify_mock_calls()
 
     def test_daemon_loop(self):
-        reply2 = {'current': set(['tap0']),
-                  'added': set(['tap2']),
-                  'removed': set([])}
+        reply_ge_1 = {'added': set(['tap0']),
+                      'removed': set([])}
 
-        reply3 = {'current': set(['tap2']),
-                  'added': set([]),
+        reply_ge_2 = {'added': set([]),
                   'removed': set(['tap0'])}
+
+        reply_pe_1 = {'current': set(['tap0']),
+                      'added': set(['tap0']),
+                      'removed': set([])}
+
+        reply_pe_2 = {'current': set([]),
+                      'added': set([]),
+                      'removed': set(['tap0'])}
+
+        reply_ancillary = {'current': set([]),
+                           'added': set([]),
+                           'removed': set([])}
 
         self.mock_int_bridge_expected += [
             mock.call.check_canary_table(),
@@ -556,7 +566,7 @@ class TunnelTest(object):
         with mock.patch.object(log.KeywordArgumentAdapter,
                                'exception') as log_exception,\
                 mock.patch.object(self.mod_agent.OVSNeutronAgent,
-                                  'scan_ports') as scan_ports,\
+                                  'process_ports_events') as process_p_events,\
                 mock.patch.object(
                     self.mod_agent.OVSNeutronAgent,
                     'scan_ancillary_ports') as scan_ancillary_ports,\
@@ -574,11 +584,16 @@ class TunnelTest(object):
                     'update_stale_ofport_rules') as update_stale:
             log_exception.side_effect = Exception(
                 'Fake exception to get out of the loop')
-            scan_ports.side_effect = [reply2, reply3]
             scan_ancillary_ports.return_value = {
                 'current': set([]), 'added': set([]), 'removed': set([]),
             }
             update_stale.return_value = []
+            devices_not_ready = set()
+            process_p_events.side_effect = [
+                (reply_pe_1, reply_ancillary, devices_not_ready),
+                (reply_pe_2, reply_ancillary, devices_not_ready)]
+            interface_polling = mock.Mock()
+            interface_polling.get_events.side_effect = [reply_ge_1, reply_ge_2]
             process_network_ports.side_effect = [
                 False, Exception('Fake exception to get out of the loop')]
             process_anc_ports.return_value = False
@@ -589,7 +604,7 @@ class TunnelTest(object):
             # We start method and expect it will raise after 2nd loop
             # If something goes wrong, assert_has_calls below will catch it
             try:
-                n_agent.daemon_loop()
+                n_agent.rpc_loop(interface_polling)
             except Exception:
                 pass
 
@@ -597,17 +612,15 @@ class TunnelTest(object):
             # messages
             log_exception.assert_called_once_with(
                 "Error while processing VIF ports")
-            scan_ports.assert_has_calls([
-                mock.call(set(), True, set()),
-                mock.call(set(['tap0']), False, set())
+            process_p_events.assert_has_calls([
+                mock.call(reply_ge_1, set(), set(), devices_not_ready, set()),
+                mock.call(reply_ge_2, set(['tap0']), set(), devices_not_ready,
+                          set())
             ])
             process_network_ports.assert_has_calls([
                 mock.call({'current': set(['tap0']),
                            'removed': set([]),
-                           'added': set(['tap2'])}, False),
-                mock.call({'current': set(['tap2']),
-                           'removed': set(['tap0']),
-                           'added': set([])}, False)
+                           'added': set(['tap0'])}, False),
             ])
 
             self.assertTrue(update_stale.called)
